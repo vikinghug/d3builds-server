d0577555b4f90e3009248c3414bd49ced01741af
Kevin Altman <kevin@itsthatguy.com>
2016-01-22 17:26:07 -0500

    removes test unit, adds rspec

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,7 @@ Rails.application.routes.draw do
   resources :skill_categories
   resources :skill_categories
 
-  resources :classes, as: :klasses, controller: :klasses do
-    resources :skill_categories, only: [:index]
-    resources :passive_skills, only: [:index, :show]
-    resources :skills, only: [:index, :show] do
-      resources :runes, only: [:index, :show]
-    end
-  end
+  resources :classes, as: :klasses, controller: :klasses
 
   resources :skills
   resources :runes
