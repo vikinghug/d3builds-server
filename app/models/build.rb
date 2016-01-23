f2855fca4be3a9639db5e0499d763897b07b85d5
Mark McEahern <mark@mceahern.com>
2016-01-22 19:01:56 -0600

    Test validation of build skill_slots limit

--- app/models/build.rb
+++ app/models/build.rb
@@ -1,9 +1,19 @@
 class Build < ActiveRecord::Base
+  SKILL_SLOT_LIMIT = 6
+
   has_many :skill_slots
   has_many :skills, through: :skill_slots
   has_many :runes, through: :skill_slots
 
   has_many :passive_skills
+
+  validate :limit_number_of_skill_slots
+
+  def limit_number_of_skill_slots
+    if skill_slots.length > SKILL_SLOT_LIMIT
+      errors.add(:skill_slots, "can't have more than #{SKILL_SLOT_LIMIT}")
+    end
+  end
 
   def to_param
     "#{id}-#{title}".parameterize
