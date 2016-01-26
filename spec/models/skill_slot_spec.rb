b5f9d482c6da5049b6a7f5dcced756ca2a4ec5df
Kevin Altman <kevin@itsthatguy.com>
2016-01-26 12:36:17 -0500

    hooks up content-type detection for build

--- spec/models/skill_slot_spec.rb
+++ spec/models/skill_slot_spec.rb
@@ -1,5 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe SkillSlot, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#build' do
+    let(:build) { create :build }
+    let(:skill_slot) { FactoryGirl.build :skill_slot, build: build }
+
+    context 'when there are less than 6 skill_slots' do
+      before do
+        create_list :skill_slot, 5, build: build
+      end
+
+      it 'is valid' do
+        expect(skill_slot).to have_valid(:build_id)
+      end
+    end
+
+    context 'when there are exactly 6 skill_slots' do
+      before do
+        create_list :skill_slot, 6, build: build
+      end
+
+      it 'is not valid' do
+        expect(skill_slot).not_to have_valid(:build_id)
+      end
+    end
+
+    context 'when there are more than 6 skill_slots' do
+      before do
+        6.times do
+          FactoryGirl
+            .build(:skill_slot, build: build)
+            .save!(validate: false)
+        end
+      end
+
+      it 'is not valid' do
+        expect(skill_slot).not_to have_valid(:build_id)
+      end
+    end
+  end
 end
